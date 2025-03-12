fcd211ad481ccf583036efbe80f1095ac00d9b3f
PabloAldo <pabloaldo24@gmail.com>
2025-03-12 16:55:30 -0600

    Modificación a funcion agregarAmigo

--- app.js
+++ app.js
@@ -5,48 +5,45 @@ let amigos = [];
 
 //funcion para agregar amigos
 function agregarAmigo() {
-    let nombreAmigo = document.getElementById('amigo').value; //Obtiene el valor de campo de entrada
-    if(nombreAmigo === ''){ //Valida si la entrada esta vacia
+    let inputAmigo = document.getElementById('amigo');
+    let nombreAmigo = inputAmigo.value;
+    if(!nombreAmigo){ //Valida si la entrada esta vacia
         alert("Por favor, inserte  un nombre.");
-    } else{
-        amigos.push(nombreAmigo);//agrega el valor al array        
-        actualizaLista();          
+        return;
     }
-    document.getElementById('amigo').value = '';//limpia el campo de entrada
+    amigos.push(nombreAmigo);//agrega el valor al array    
+    inputAmigo.value = '';//limpia el campo de entrada
+    inputAmigo.focus();    
+    actualizaLista();    
 }   
 
 //actualiza la lista de amigos en pantalla
 function actualizaLista(){    
-    let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = " ";
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = " ";
+    
     for (let i = 0; i < amigos.length; i++) {
-        let li = document.createElement("li");        
-        li.textContent = amigos[i];
-        lista.appendChild(li);
+        let item = document.createElement("li");        
+        item.textContent = amigos[i];
+        listaAmigos.appendChild(item);
     }
 }
 
 //selecciona de manera aleatoria un amigo
 function sortearAmigo(){
+
     if(amigos.length > 0){        
-        let numIndice = Math.floor(Math.random() * amigos.length);
-        console.log(numIndice);        
-        let resultado = document.getElementById('resultado');
-        let li = document.createElement("li");        
-        li.textContent = `El amigo secreto es ${amigos[numIndice]}`;
-        resultado.appendChild(li);
-        inicio();       
+        let numIndice = Math.floor(Math.random() * amigos.length);               
+        let resultado = document.getElementById('resultado');              
+        resultado.innerHTML = `El amigo secreto es ${amigos[numIndice]}`;       
+        let lista = document.getElementById('listaAmigos');
+        lista.innerHTML = " ";
+        amigos = [] 
+
     } else{
         alert("No ha ingresado el nombre de ningun amigo.");
         resultado.innerHTML = " ";
     }
-}
-
-//Inicio de pagina
-function inicio(){
-    let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = " ";
-    amigos = []    
 }
 
 
